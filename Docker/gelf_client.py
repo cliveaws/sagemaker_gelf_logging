162eb4c2a198bfb80c5a99785725a2ce67a9a154
Clive Davies <davesmn@amazon.co.uk>
2022-12-15 15:05:18 +0000

    clean up gelf client

--- Docker/gelf_client.py
+++ Docker/gelf_client.py
@@ -4,15 +4,11 @@ import sys
 import time
 import os
 
-
-
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
-#logger.addHandler(GelfTcpHandler(host='127.0.0.1', port=9401))
+logger.propagate = False
+
 logger.addHandler(GelfUdpHandler(host=os.environ['GELF_LOGGING_HOST'], port=12201))
-#logger.addHandler(GelfTlsHandler(host='127.0.0.1', port=9403))'
-#logger.addHandler(GelfHttpHandler(host='127.0.0.1', port=9404))
 
 logger.info(f"Hello from gelf client, logging to {os.environ['GELF_LOGGING_HOST']}")
 print(f"Hello from gelf client, logging to {os.environ['GELF_LOGGING_HOST']}")
